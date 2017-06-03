a846f2e926d9322db1804da921dfe89810b834e2
¨s1250042-Watanabe¨ <s1250042@u-aizu.ac.jp>
2017-06-03 20:25:21 +0900

    fix 02_expression/two_rectangles_of_area

--- src/02_expression/two_rectangles_of_area.rb
+++ src/02_expression/two_rectangles_of_area.rb
@@ -1,3 +1,14 @@
 def two_rectangles_of_area(x1, y1, x2, y2, a1, b1, a2, b2)
-  (x2 - x1) * (y2 - y1) + (a2 - a1) * (b2 - b1)
+
+  s = (x2 - x1) * (y2 - y1) + (a2 - a1) * (b2 - b1)
+  
+  w = [x2, a2].min - [x1, a1].max
+  h = [y2, b2].min - [y1, b1].max
+
+  if w > 0 && h > 0 then
+    s -= w * h
+  end
+  
+  s
+  
 end
