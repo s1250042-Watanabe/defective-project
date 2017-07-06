618bff8cfd4f03ba17c650b5d94ec105afd73d84
¨s1250042-Watanabe¨ <s1250042@u-aizu.ac.jp>
2017-07-06 19:32:58 +0900

    fix 04/address_book_demo

--- src/04_array_hash/address_book_demo.rb
+++ src/04_array_hash/address_book_demo.rb
@@ -1,2 +1,21 @@
 def address_book(book, names)
+  
+  str =""
+  names.each {|name|
+    
+    hash = {name: 'unknown', children: 0, role: 'unknown'}
+    
+    book[name].each{ |key,value|
+      hash[key] = value
+    }
+    
+    hash.each{ |key, value|
+      str = str + value.to_s + ", "
+    }
+    str.slice!(-2..-1)
+    str += "\n"
+  }
+  str.slice!(-1)
+  str
+  
 end
